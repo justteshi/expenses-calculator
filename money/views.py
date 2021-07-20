41c4f3462ccd24b3be956b1dc9b702406629cb44
thristov <thristov@abrites.com>
2021-07-20 17:05:28 +0300

    Feat: today total expenses

--- money/views.py
+++ money/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import ExpenseArticle
+from django.db.models import Sum
+from datetime import date
 
 
 def index(request):
+    # Fetch all Expense articles
     articles = ExpenseArticle.objects.all()
     
+    # Fetch all articles of today and their sum
+    today_date = date.today()
+    articles_today = ExpenseArticle.objects.filter(date__date=today_date)
+    expenses_today = articles_today.aggregate(Sum('amount'))['amount__sum']
+
+
+
     return render(request, 'home.html',{
         'articles': articles,
+        'today_date': today_date,
+        'expenses_today': expenses_today,
     })
 
