e0e0f28905e9581af825e58d1fbc955d20562365
thristov <thristov@abrites.com>
2021-07-20 14:27:18 +0300

    Add: Tailwind css and homepage UI

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
+
 
 urlpatterns = [
     path('', include('money.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
